2cdb379db5416fed796f6276e72f0fed4234db47
Jota Brito <ermaojota@gmail.com>
2019-02-03 00:26:34 -0200

    Tornando a conexão com o mongo reaproveitável

--- back/db.js
+++ back/db.js
@@ -1,59 +1,37 @@
-MongoClient = require('mongodb').MongoClient;
-
-//const server = 'mongodb://localhost:27017';
+// const server = 'mongodb://localhost:27017';
 const server = 'mongodb+srv://healthy:tcc@healthy-eb2wc.mongodb.net/test?retryWrites=true';
-const database = 'healthy';
+const databaseName = 'healthy';
 
-MongoClient.connect(server, { useNewUrlParser: true }, function (err, conn) {
-    if (err) {
-        console.log(err);
-        global.db = undefined;
-    } else {
-        global.db = conn.db(database);
-    }
-});
+var database;
+
+function conectar() {
+    let conexao;
+
+    require('mongodb').MongoClient.connect(server, { useNewUrlParser: true }, function (err, conn) {
+        if (err) {
+            database = undefined;
+        } else {
+            conexao = conn;
+            database = conn.db(databaseName);
+        }
+    });
+
+    process.on('SIGINT', () => {
+        conexao.close();
+        process.exit();
+      });
+}
 
 function get(collection) {
     if (!collection) {
-        return global.db;
+        return database;
     } else {
-        return global.db.collection(collection);
+        return database.collection(collection);
     }
 }
 
-function find(collection, value, index, obj) {
-    var documentos;
-    global.db.collection(collection).find(value, index, obj)
-    .toArray(function(err, docs) {
-        if (err) {
-            console.log(err);
-            return undefined;
-        } else {
-            return docs;
-        }
-    })
-}
-
-function findMedicamentos(callback) {
-    global.db.collection('medicamentos').find().toArray(function(err, docs) {
-        if (err) console.log(err);
-        callback(docs);
-    })
-}
-
-// function get(collection, callback) {
-//     global.db.collection(collection).find().toarray(function(err, docs) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             callback(docs);
-//             return 'isso é um retorno.';
-//         }
-//     });
-// }
+conectar();
 
 module.exports = {
-    get,
-    find,
-    findMedicamentos
+    get
 }
