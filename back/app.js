ed327a010274398bb0bc3a1baedfdcc28d41dfc3
Jota Brito <ermaojota@gmail.com>
2019-02-04 14:08:31 -0200

    Preparando arquivos para publicação

--- back/app.js
+++ back/app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../front/dist/front/')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', indexRouter);
 app.use('/api/v1/users', usersRouter);
